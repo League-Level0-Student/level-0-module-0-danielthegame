a6a80ad2c80bc0b95c1718c8278a92ebcc9aaf14
i-lab user <i-labuser@SDPL88535-iMacProLO.local>
2019-10-15 17:56:55 -0700

    Update KnockKnock.java

--- src/_04_knock_knock/KnockKnock.java
+++ src/_04_knock_knock/KnockKnock.java
@@ -4,7 +4,25 @@ import javax.swing.JOptionPane;
 
 public class KnockKnock {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"Knock knock how is there boo boo how dony need to cry it is just a joke");
+		JOptionPane.showMessageDialog(null,"Knock knock");
 	}
+	
+	JOptionPane.showMessageDialog(null,"how is there");	
+	
+	JOptionPane.showMessageDialog(null,"boo");	
+	
+	JOptionPane.showMessageDialog(null,"boo how");	
+	
+	JOptionPane.showMessageDialog(null,"boo how");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
