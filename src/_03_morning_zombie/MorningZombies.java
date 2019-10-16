75c1edb5269d713bcca122e3151108b80400ccc5
i-lab user <i-labuser@SDPL88535-iMacProLO.local>
2019-10-15 17:53:59 -0700

    knock knock joke
    kno

--- src/_03_morning_zombie/MorningZombies.java
+++ src/_03_morning_zombie/MorningZombies.java
@@ -1,28 +1,32 @@
 package _03_morning_zombie;
 
+import javax.swing.JOptionPane;
+
 public class MorningZombies {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		JOptionPane.showMessageDialog(null,"WAKE UP");
+	
+		
+		JOptionPane.showMessageDialog(null,"EAT BREAKFAST");
+			
+		
+		JOptionPane.showMessageDialog(null,"BRUSH YOUR TEETH");	
+				
+		
+		JOptionPane.showMessageDialog(null,"PUT GEL ON YOUR HAIR");
+		
+		
+		JOptionPane.showMessageDialog(null,"GET MY BACKPACK");
 		
 		
 		
+		JOptionPane.showMessageDialog(null,"GET IN THE CAR");
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		JOptionPane.showMessageDialog(null,"PUT YOUR SET BELT");
 		
 		
 		
---
+++ src/_04_knock_knock/KnockKnock.java
@@ -0,0 +1,10 @@
+package _04_knock_knock;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Knock knock how is there boo boo how dony need to cry it is just a joke");
+	}
+
+}
