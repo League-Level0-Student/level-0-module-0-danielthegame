758bc17ef07c2c20aa732f38df68df0578fd0c65
i-lab user <i-labuser@SDPL88539-iMacProLO.local>
2019-10-29 17:55:40 -0700

    every thing is awsome

--- src/EveryThingisAwsome.java
+++ src/EveryThingisAwsome.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 
 String input=	JOptionPane.showInputDialog(null,"what do you like");
 
-
+JOptionPane.showMessageDialog(null,input + " is awsome ");
 
 
 
--- src/lastSummer.java
+++ src/lastSummer.java
@@ -1,13 +1,29 @@
+import javax.swing.JOptionPane;
 
 public class lastSummer {
-public static void main(String[] args) {
-	JOptionPane.showInputDialog(null,"what did you do last summer");
-	JOptionPane.showMessageDialog	
+public static void main(String[] args) { 
+
+	String answer=JOptionPane.showInputDialog(null,"what did you do last summer");
+	JOptionPane.showMessageDialog(null,"I know you "+answer+" last summer");
 	
+}	
+}	
 	
-	
-	
-} 
+		
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
